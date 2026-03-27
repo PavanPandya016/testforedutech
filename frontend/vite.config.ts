e88820da731005b716f4d78f03aad686fc3e198f
Pavan <pavanpandya016@gmail.com>
2026-03-27 18:53:42 +0530

    build fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   assetsInclude: ['**/*.svg', '**/*.csv'],
   
   build: {
-    outDir: '../backend/build',
+    outDir: 'build',
     rollupOptions: {
       output: {
         manualChunks: {
